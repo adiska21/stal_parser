c979e5a0265a865aa857d1db335c397150aa23cd
adiska21 <adislan2017@gmail.com>
2025-03-25 04:58:49 +0300

    init of stalenegro

--- stalenegro_parse/main.py
+++ stalenegro_parse/main.py
@@ -1,6 +1,11 @@
 import requests
-from bs4 import BeautifulSoup
-import fake_useragent
-import selenium
+from bs4 import BeautifulSoup as BS
+# import fake_useragent
+# import selenium
 
 
+LINK = "https://stalenergo-96.ru/produkcia/"
+response = requests.get(LINK).text
+soup = BS(response, "lxml").find("div", class_="catalogue_content").find_all("div", class_="catalogue__item")
+
+print(soup)
